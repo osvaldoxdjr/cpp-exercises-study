526156bae05f00679412ba8c8fc56f511551f788
Osvaldo Xavier <osvaldoxdjr@gmail.com>
2018-07-10 22:25:32 -0300

    Add sum program

---
+++ exercises/Sum/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float n1, n2;
+    cout << "Digite o primeiro numero:" << endl;
+    cin >> n1;
+    cout << "Digite o segundo numero:" << endl;
+    cin >> n2;
+    cout << "A soma dos numeros eh: " << n1+n2 << endl;
+return 0;
+}
